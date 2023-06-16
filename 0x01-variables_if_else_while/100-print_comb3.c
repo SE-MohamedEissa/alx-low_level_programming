7a181800fb57d0f66bd4b082197d04fdb3f77b4f
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-06-15 20:28:54 -0700

    Mohamed

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0;
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+		for (j = i; j <= 57; j++)
+		{
+			putchar(i), putchar(j);
+			if (i != 56 || j != 57)
+				putchar(','), putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
