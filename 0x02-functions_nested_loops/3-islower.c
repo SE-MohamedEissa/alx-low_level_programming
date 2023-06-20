adb8db9c907372e59578cc47aeab892a45014b41
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-06-21 01:14:15 +0300

    Mohamed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - check
+ * @c: char
+ * Return: 0 or 1
+ */
+int _islower(int c);
+{
+	char i;
+	int flag = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i == c)
+			flag = 1;
+	return (low);
+}
