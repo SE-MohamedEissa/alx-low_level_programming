2b844294305fb09dc007a21398c1ffba7e8510eb
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-06-27 00:57:53 +0300

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,16 +8,16 @@
  * @src: source.
  * Return: the pointer to dest.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *d, char *src)
 {
 	int count = 0;
 
 	while (count >= 0)
 	{
-		*(dest + count) = *(src + count);
+		*(d + count) = *(src + count);
 		if (*(src + count) == '\0')
 			break;
 		count++;
 	}
-	return (dest);
+	return (d);
 }
