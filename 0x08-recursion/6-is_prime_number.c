3be5e09ccc475407732a35d928a84ea2b30f6254
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-08-30 21:52:02 +0300

    prime check

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,27 @@
 #include "main.h"
 /**
- * is_prime_number - check prime
+ * primee - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int primee(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (primee(n, i - 1));
+}
+/**
+ * is_prime_number - check if number prime
  * @n: number
- * Return: 1 if prime 0 if not
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
-		return (1);
-	for (i = 2; i * i <= n; ++i)
-		if (n % i == 0)
-			return (0);
-	return (1);
+		return (0);
+	return (primee(n, n - 1));
 }
