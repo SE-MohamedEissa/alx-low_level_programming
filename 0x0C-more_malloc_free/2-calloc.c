00401dd1f0b47cd5fb731e682a07d99bc342de74
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-09-08 03:53:49 +0300

    2rabt a5las

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_calloc - allocate memory for array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; ++i)
+		*(ptr + i) = 0;
+	return (ptr);
+}
