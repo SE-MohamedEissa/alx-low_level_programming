f05a917f3cfdab1805712f8779651076daaa2d72
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-09-16 11:48:23 +0300

    i think thats look better
    q
    q
    q!
    ]
    A]
    d.f'
    ,; ;m
    ;lmmpm
    pm
    pon1n
    n
    ;
    I

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,41 +8,31 @@
 void print_all(const char * const format, ...)
 {
 	va_list all;
-	unsigned int i = 0, j, k = 0;
-	char *ptr;
+	unsigned int i = 0, j = 0, k = 0;
+	char *ptr, *c = ", ";
 	const char form[] = "cifs";
 
 	va_start(all, format);
+	while (format && format[j])
+		j++;
 	while (format && format[i])
 	{
-		j = 0;
-		while (form[j])
-		{
-			if (format[i] == form[j] && k)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
+		if (i == j - 1)
+			c = "";
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(all, int)), k = 1;
+			printf("%c%s", va_arg(all, int), c);
 			break;
 		case 'i':
-			printf("%d", va_arg(all, int)), k = 1;
+			printf("%d%s", va_arg(all, int), c);
 			break;
 		case 'f':
-			printf("%f", va_arg(all, double)), k = 1;
+			printf("%f%s", va_arg(all, double), c);
 			break;
 		case 's':
-			ptr = va_arg(all, char *), k = 1;
-			if (!ptr)
-			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s", ptr);
+			ptr = va_arg(all, char *);
+			printf("%s%s", ptr ? ptr : "(nil)", c);
 			break;
 		} i++;
 	}
