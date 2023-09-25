c2af6da6f3cd00c002312b8325b38366332da1c0
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-09-26 02:42:53 +0300

    9th task

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,12 +9,12 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
-	if(!head)
+	if (!head)
 		return (0);
 	while (head)
 	{
 		sum += head->n;
-		head = head ->next;
+		head = head->next;
 	}
 	return (sum);
 }
