afaae2e71d506c6c9f9a16055f0b5e368ddd5f86
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-09-26 02:37:53 +0300

    8th task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  * get_nodeint_at_index - return the nth node
@@ -8,12 +8,11 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node = head;
 	unsigned int i = 0;
 
-	while (i++ < index && node)
-		node = node->next;
-	if (!node)
+	while (i++ < index && head)
+		head = head->next;
+	if (!head)
 		return (NULL);
-	return (node);
+	return (head);
 }
